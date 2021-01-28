b4bec078092975a3927659dc803359df53429fee
Pascal <41127800+PasquinelP@users.noreply.github.com>
2021-01-28 16:47:17 +0100

    Add files via upload

---
+++ opdracht-if-else-statements/script.js
@@ -0,0 +1,21 @@
+const age = 20;
+const isFemale = true;
+const driverStatus = "bob";
+
+if (age >= 18) {
+  console.log("Welkom, kom binnen");
+} else {
+  console.log("Helaas, je bent te jong. Blijf buiten.");
+}
+
+if (isFemale) {
+  console.log("Veel plezier bij onze ladies night!");
+} else {
+  console.log("Helaas, deze avond is alleen voor vrouwen. Zien we je een andere avond?");
+}
+
+if(driverStatus == "bob") {
+  console.log("Jij bent de bob. Breng iedereen veilig thuis en rij voorzichtig");
+} else {
+  console.log("Je mag niet rijden. Zoek de bob en hij of zij brengt je naar huis.");
+}
